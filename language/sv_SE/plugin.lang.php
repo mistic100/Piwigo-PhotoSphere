ad1a01ccdbec1c50fb8376ec443780f620c1165d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-12 20:32:26 +0200

    Add sv_SE, thanks to wahlis

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Set Photo Sphere'] = 'Markera bilden som fotosfär';
+$lang['Unset Photo Sphere'] = 'Markera bilden som vanlig bild';
+$lang['photosphere_config_intro'] = 'För att använda Photo Sphere på en bild, markera "Photo Sphere" på bildens egenskapssida. Du kan också använda batch hanteraren. Och du kan även använda Snabbredigering under Adminsitrationsverktygen';
+$lang['Automatically launch animation'] = 'Starta animeringen automatisk';
+$lang['Between 1024px and 8192px.'] = 'Mellan 1024 och 8192 pixlar';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Större texturer tar längre tid att ladda men mindre ger ett suddigare resultat';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Visa Photo Spheres ikon i miniatyrlistan';
+$lang['Display help message'] = 'Visa hjälpmeddelande';
+$lang['Drag and drop to navigate in the photo.'] = 'Dra och släpp för att navigera i bilden';
+$lang['Loading'] = 'Laddar';
+$lang['Sphere texture size'] = 'Sfärens texturstorlek';
