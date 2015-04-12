bf8f01b58406acc372dc8ed4349ae29f93f6f633
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-12 13:09:31 +0200

    Add sk_SK, thanks to dodo

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['photosphere_config_intro'] = 'Ak chcete použiť sférickú fotku ako fotografiu, zaškrtnite "Photo Sphere" na stránke jej vlastností. Môžete tiež použiť dávkového správcu. A môžete tiež použiť Quick Edit a nástroje pre správu doplnkov.';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Väčšiu textúru sa bude dlhšie načítavať, ale menšie textúry budú mať za následok rozmazané sféry.';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Zobrazenie ikony sférickej fotky v zozname náhľadov';
+$lang['Drag and drop to navigate in the photo.'] = 'Drag and drop pre navigáciu fotky.';
+$lang['Unset Photo Sphere'] = 'Vymazať sférickú fotku';
+$lang['Sphere texture size'] = 'Veľkosť sférickej textúry';
+$lang['Set Photo Sphere'] = 'Nastaviť sférickú fotku';
+$lang['Loading'] = 'Nahrávanie';
+$lang['Display help message'] = 'Zobrazí nápovedu';
+$lang['Between 1024px and 8192px.'] = 'Medzi 1024px a 8192px.';
+$lang['Automatically launch animation'] = 'Automaticky spustí animáciu';
