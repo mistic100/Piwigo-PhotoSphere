0435fef4a0b5da06954408582658ded44cb2652d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-13 06:29:13 +0200

    Add ru_RU, thanks to Konve

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Automatically launch animation'] = 'Автоматический запуск анимации';
+$lang['Between 1024px and 8192px.'] = 'Между 1024px и 8192px.';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Большие текстуры займут больше времени для загрузки, но уменьшение текстур приведет к размытию сферы.';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Отображение иконки Photo Sphere в списке миниатюр';
+$lang['Display help message'] = 'Отображение подсказки помощи';
+$lang['Drag and drop to navigate in the photo.'] = 'Перетаскивание (Drag and drop) для перемещения по фотографии.';
+$lang['Loading'] = 'Загрузка';
+$lang['Set Photo Sphere'] = 'Установки Photo Sphere';
+$lang['Sphere texture size'] = 'Размер текстуры сферы';
+$lang['Unset Photo Sphere'] = 'Сброс установок Photo Sphere';
+$lang['photosphere_config_intro'] = 'Для использования Photo Sphere на фото, поставьте галочку в поле "Photo Sphere" на странице свойств. Вы также можете применять пакетный менеджер. А еще можно работать через "Быстрое редактирование" в плагине Admin Tools.';
