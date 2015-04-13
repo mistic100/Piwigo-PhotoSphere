5b7b0c1dce383fc589f4ef4dd821559ae30c4e04
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-13 18:40:56 +0200

    Add es_ES, thanks to jpr928

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['photosphere_config_intro'] = 'Para utilizar Photo Sphere en una foto, utiliza la pagina de propriedades de Photo Sphere. Puede tambien utilizar el administrador de lotes. Y tambien el plugin de las heramientas de administrador "Quick Edit".';
+$lang['Unset Photo Sphere'] = 'Desconfigurar Photo Sphere';
+$lang['Sphere texture size'] = 'Tamaño de la textura de Sphere';
+$lang['Set Photo Sphere'] = 'Configurar Photo Sphere';
+$lang['Loading'] = 'Cargando';
+$lang['Drag and drop to navigate in the photo.'] = 'Seleccionar y soltar para navegar en la photo';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Mostrar el icono de Photo Sphere en la lista de miniaturas';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Texturas más grandes tardan más en cargar pero texturas menores crean esfera borrosa.';
+$lang['Automatically launch animation'] = 'Iniciará automáticamente la animación';
+$lang['Display help message'] = 'Mostrar mensaje de ayuda ';
+$lang['Between 1024px and 8192px.'] = 'Entre 1024px y 8192px.';
