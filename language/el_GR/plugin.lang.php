8f157219d4bcfd5efdde55317cdb76840845c77e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-13 18:40:48 +0200

    Add el_GR, thanks to bas_alba

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['photosphere_config_intro'] = 'Για να χρησιμοποιήσετε το Photo Sphere σε μια φωτογραφία, επιλέξτε "Photo Sphere" στη σελίδα με τις ιδιότητες. Μπορείτε επίσης να χρησιμοποιήσετε τον διαχειριστή παρτίδας. Και μπορείτε επίσης να χρησιμοποιήσετε το πρόσθετο Quick Edit στα Admin Tools.';
+$lang['Unset Photo Sphere'] = 'Απενεργοποίηση Photosphere';
+$lang['Sphere texture size'] = 'Μέγεθος υφής Σφαίρας';
+$lang['Set Photo Sphere'] = 'Ρύθμιση του Photo Sphere';
+$lang['Loading'] = 'Φόρτωση';
+$lang['Drag and drop to navigate in the photo.'] = 'Drag and drop για να πλοηγηθείτε στη φωτογραφία.';
+$lang['Display help message'] = 'Εμφάνιση μηνύματος βοήθειας';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Η Εμφάνιση του εικονιδίου του Photosphere στη λίστα μικρογραφιών.';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Με μεγαλύτερες υφές θα χρειαστεί περισσότερος χρόνος για να φορτώσει, αλλά με μικρότερες υφές το αποτέλεσμα θα είναι μια θολή σφαίρα.';
+$lang['Automatically launch animation'] = 'Αυτόματη έναρξη κινούμενων σχεδίων';
+$lang['Between 1024px and 8192px.'] = 'Μεταξύ 1024px και 8192px.';
