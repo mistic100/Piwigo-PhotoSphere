e2482aeafe335c93d0fdd72e29e98dfb6223b691
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-13 06:29:17 +0200

    Add da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Automatically launch animation'] = 'Start animation automatisk';
+$lang['Between 1024px and 8192px.'] = 'Mellem 1024 og 8192 pixel.';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Større teksturer tager længere tid at indlæse, mens mindre teksturer medfører en uskarp kugle.';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Vis Photo Sphere-ikon på listen over miniaturebilleder.';
+$lang['Display help message'] = 'Vis hjælpetekster';
+$lang['Drag and drop to navigate in the photo.'] = 'Træk og slip for at navigere i fotografiet.';
+$lang['Loading'] = 'Indlæser';
+$lang['Set Photo Sphere'] = 'Sæt Photo Sphere';
+$lang['Sphere texture size'] = 'Størrelse på kuglens tekstur';
+$lang['Unset Photo Sphere'] = 'Fjern opsætning af Photo Sphere';
+$lang['photosphere_config_intro'] = 'For at anvende Photo Sphere på et fotografi, vælges "Photo Sphere" på dens egenskabsside.  Alternativt kan Batch Manager benyttes.  Desuden kan man anvende Hurtigredigering på siden med administrationsværktøjer.';
