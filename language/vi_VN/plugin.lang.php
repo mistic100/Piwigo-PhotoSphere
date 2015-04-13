e97309cffd278c542dcf9e9217ce49cd563b8a8e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-13 18:40:59 +0200

    Add vi_VN, thanks to dummy

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Automatically launch animation'] = 'Tự động chạy hoạt ảnh';
+$lang['Between 1024px and 8192px.'] = 'Giữa 1024px và 8192px.';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Họa tiết lớn sẽ tải lâu hơn nhưng họa tiết nhỏ hơn sẽ làm Quả cầu bị mờ.';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Hiện biểu tượng Quả cầu ảnh ở danh sách ảnh đại diện';
+$lang['Display help message'] = 'Hiện thông báo giúp đỡ';
+$lang['Drag and drop to navigate in the photo.'] = 'Kéo và thả để thao tác trong hình ảnh.';
+$lang['Loading'] = 'Đang tải';
+$lang['Set Photo Sphere'] = 'Đặt Quả cầu ảnh';
+$lang['Sphere texture size'] = 'Kích thước chữ Quả cầu';
+$lang['Unset Photo Sphere'] = 'Gỡ bỏ Quả cầu ảnh';
+$lang['photosphere_config_intro'] = 'Để sử dụng Quả cầu ảnh cho một ảnh, đánh dấu vào "Quả cầu ảnh" trong trang thuộc tính. Bạn cũng có thể dùng trình quản lý đa tác vụ hay chức năng Sửa nhanh trong plugin Công cụ quản trị.';
