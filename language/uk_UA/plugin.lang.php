ea32195509c9fbde198624cdc0eeb576f96a5b81
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-14 12:23:54 +0200

    Add uk_UA, thanks to m_alien

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['photosphere_config_intro'] = 'Для використання Photo Sphere на світлині, перевірте активність "Photo Sphere" на її сторінці налаштувань. Ви також можете викоритовувати пакетний менеджер. Також ви можете використовувати швидке редагування з плагіну Admin Tools.';
+$lang['Unset Photo Sphere'] = 'Убрати Photo Sphere';
+$lang['Set Photo Sphere'] = 'Встановити Photo Sphere';
+$lang['Sphere texture size'] = 'Розмір текстури сфери';
+$lang['Drag and drop to navigate in the photo.'] = 'Перетягніть для переміщення по фотографії.';
+$lang['Display help message'] = 'Показувати повідомлення підказки';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Показувати іконку Фотосфери у переліку мініатюр';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Великі текстури будуть довше завантажуватися але маленькі текстури призведуть до розмиття сфери.';
+$lang['Between 1024px and 8192px.'] = 'Поміж 1024px та 8192px.';
+$lang['Loading'] = 'Завантаження';
+$lang['Automatically launch animation'] = 'Автоматично запускати анімацію';
