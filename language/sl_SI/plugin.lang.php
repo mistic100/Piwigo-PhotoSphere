2407276148cb96cd26f31d055373543fe2d99cfc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-05-02 12:41:33 +0200

    Add sl_SI, thanks to MarkoAm

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Automatically launch animation'] = 'Samodejno začni animacijo';
+$lang['Between 1024px and 8192px.'] = 'Med 1024px in 8192px';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Večje teksture rabijo dalj časa za nalaganje, manjše pa bodo v sferi zgledale zamegljeno';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Prikaži ikono Sfera slike (Photo Sphere) na seznamu sličic';
+$lang['Display help message'] = 'Prikaži sporočilo pomoči';
+$lang['Drag and drop to navigate in the photo.'] = 'Povleci in spusti za premikanje v sliki';
+$lang['Loading'] = 'Nalaganje';
+$lang['Set Photo Sphere'] = 'Nastavi Sfero slike (Photo Sphere)';
+$lang['Sphere texture size'] = 'Velikost teksture v sferi';
+$lang['Unset Photo Sphere'] = 'Odstrani nastavitev Sfere slike';
+$lang['photosphere_config_intro'] = 'Da bi uporabili Sfero slike na pripadajoči strani lastnosti slike označite "Sfera slike". Uporabite lahko tudi paketni urejevalnik ali pa hitri urejevalnik med vtičniki Admin orodij.';
