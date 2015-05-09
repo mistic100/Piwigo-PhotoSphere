b26fac22941ac080d1f1890afff4dad1c25895ef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-05-10 00:59:33 +0200

    Add nb_NO, thanks to abbamust

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Between 1024px and 8192px.'] = 'Mellom 1024px og 8192px';
+$lang['Display help message'] = 'Vis hjelpebeskjed';
+$lang['Fullscreen'] = 'Fullskjerm';
+$lang['Loading'] = 'Laster';
+$lang['Zoom'] = 'Zoom';
+$lang['Zoom in'] = 'Zoom inn';
+$lang['Zoom out'] = 'Zoom ut';
+$lang['Automatic rotation'] = 'Automatisk rotasjon';
+$lang['Automatically launch animation'] = 'Start animasjoner automatisk';
