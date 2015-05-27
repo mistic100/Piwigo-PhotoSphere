36c0bee383376c90266975549458434e37b3f9db
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-05-27 20:29:29 +0200

    Add sr_RS, thanks to gpopac

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Automatic rotation'] = 'Сам окрени слику';
+$lang['Automatically launch animation'] = 'Сам покрени анимацију';
+$lang['Between 1024px and 8192px.'] = 'Између 1024px и 8192px.';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Веће текстуре се спорије учитавају док ће мање изгледати замућено када отворите сферу.';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Прикажи иконицу Фото-сфере преко сличица';
+$lang['Display help message'] = 'Прикажи поруку за помоћ';
+$lang['Drag and drop to navigate in the photo.'] = 'Превуците приказ да се прошетате кроз слику.';
+$lang['Fullscreen'] = 'Цео екран';
+$lang['Loading'] = 'Учитавам';
+$lang['Set Photo Sphere'] = 'Постави Фото-сферу';
+$lang['Sphere texture size'] = 'Величина текстуре за сферу';
+$lang['Unset Photo Sphere'] = 'Врати Фото-сферу';
+$lang['Zoom'] = 'Увећање';
+$lang['Zoom in'] = 'Увећај';
+$lang['Zoom out'] = 'Умањи';
+$lang['photosphere_config_intro'] = 'Уколико желите да користите овај додатак кликните на Фото-сферу у страницама за подешавање. Можете користити менаџер операција или брзо уређивање из администаторских алата.';
