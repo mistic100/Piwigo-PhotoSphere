434fb21196d27f47a1bd54540748c60df780c0bc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-07-09 06:13:10 +0200

    Add zh_CN, thanks to dennisyan

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['photosphere_config_intro'] = '在一张照片上应用 Photo Sphere：在其属性页上勾选 "Photo Sphere"。您也可以使用批量管理或 Admin Tools 插件中的快捷编辑功能进行设置。';
+$lang['Automatic rotation'] = '自动旋转';
+$lang['Automatically launch animation'] = '自动启用动画';
+$lang['Between 1024px and 8192px.'] = '在 1024px 至 8192px 之间。';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = '更大的纹理值将需要更长的加载时间，但更小的纹理值将使球面模糊。';
+$lang['Display Photo Sphere icon on thumbnails list'] = '在缩略图列表中显示 Photo Sphere 图标';
+$lang['Display help message'] = '显示帮助信息';
+$lang['Drag and drop to navigate in the photo.'] = '使用鼠标拖放在照片中转向。';
+$lang['Fullscreen'] = '全屏';
+$lang['Loading'] = '加载中';
+$lang['Set Photo Sphere'] = '设置 Photo Sphere';
+$lang['Sphere texture size'] = '球面纹理尺寸';
+$lang['Unset Photo Sphere'] = '重置 Photo Sphere';
+$lang['Zoom'] = '缩放';
+$lang['Zoom in'] = '放大';
+$lang['Zoom out'] = '缩小';
