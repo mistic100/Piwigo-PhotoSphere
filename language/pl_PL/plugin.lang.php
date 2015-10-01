d6cbab12e2620b1089bf628365f6cd1275e66895
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-10-01 06:46:37 +0200

    Add pl_PL, thanks to Krzysiu

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Automatic rotation'] = 'Automatyczny obrót';
+$lang['Automatically launch animation'] = 'Automatyczne uruchamianie animacji';
+$lang['Between 1024px and 8192px.'] = 'Pomiędzy 1024 a 8192 pikseli';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Większe tekstury dłużej się ładują, a mniejsze spowodują rozmycie kuli';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Pokaż ikonę Photo Sphere na liście miniaturek';
+$lang['Display help message'] = 'Pokaż tekst pomocy';
+$lang['Drag and drop to navigate in the photo.'] = 'Przeciągnij i upuść, by nawigować między zdjęciami';
+$lang['Fullscreen'] = 'Pełny ekran';
+$lang['Loading'] = 'Ładowanie';
+$lang['Set Photo Sphere'] = 'Włącz Photo Sphere';
+$lang['Sphere texture size'] = 'Rozmiar tekstury kuli';
+$lang['Unset Photo Sphere'] = 'Wyłącz Photo Sphere';
+$lang['Zoom'] = 'Powiększenie';
+$lang['Zoom in'] = 'Zbliż';
+$lang['Zoom out'] = 'Oddal';
+$lang['photosphere_config_intro'] = 'Aby użyć Photo Sphere do zdjęcia, kliknij na "Photo Sphere" na stronie właściwości. Możesz także użyć przetwarzania wsadowego lub Szybkiej Edycji dostępnej we wtyczce Admin Tools.';
