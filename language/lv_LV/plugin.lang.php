3a27b64c62484c37d4f89ae1bf0c68cfb916b065
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-12 21:50:27 +0200

    Add lv_LV, thanks to agrisans

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['photosphere_config_intro'] = 'Lai izmantotu Foto Sfŗu uz fotoattēla, pārbaudiet "Photo Sphere" tās rekvizītu lapā. Varat izmantot arī virknes pārvaldnieku, vai Quick Edit Administratora Rīku spraudnī.';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Lielākām tekstūrām būs vajadzīgs ilgāks laiks, lai ielādētos, bet mazākas tekstūras radīs neskaidru sfēru.';
+$lang['Drag and drop to navigate in the photo.'] = '"Velciet un nometiet", lai navigētu foto.';
+$lang['Loading'] = 'Ielādēšana';
+$lang['Zoom out'] = 'Attālināt';
+$lang['Zoom in'] = 'Pietuvināt';
+$lang['Zoom'] = 'Tālummaiņa';
+$lang['Sphere texture size'] = 'Sfēras tekstūras izmērs';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Rādīt Foto Sfēras ikonu sīktēlu sarakstā';
+$lang['Automatically launch animation'] = 'Automātiski palaist animāciju';
+$lang['Set Photo Sphere'] = 'Iestatīt Foto Sfēru';
+$lang['Unset Photo Sphere'] = 'Atiestatīt Foto Sfēru';
+$lang['Between 1024px and 8192px.'] = 'Starp 1024px and 8192px.';
+$lang['Fullscreen'] = 'Pilns ekrāns';
+$lang['Display help message'] = 'Rādīt palīdzības ziņojumu';
+$lang['Automatic rotation'] = 'Automātiskā pagriešana';
