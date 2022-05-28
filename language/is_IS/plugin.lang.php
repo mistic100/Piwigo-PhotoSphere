bbdd3bb37f9a774cb4874995326c73952f428136
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-28 09:28:03 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Sphere texture size'] = 'Áferðarstærð kúlu';
+$lang['Unset Photo Sphere'] = 'Afvirkja hringmynd';
+$lang['Zoom'] = 'Þysja';
+$lang['Zoom in'] = 'Þysja inn';
+$lang['Zoom out'] = 'Þysja út';
+$lang['photosphere_config_intro'] = 'Til að nota hringmynd á mynd skaltu haka við "Photo Sphere" á eiginleikasíðu myndarinnar. Þú getur líka notað lotustjórann eða flýtiritil í stjórnandatækja viðbótinni.';
+$lang['Automatic rotation'] = 'Sjálfvirkur snúningur';
+$lang['Automatically launch animation'] = 'Ræstu hreyfimynd sjálfkrafa';
+$lang['Between 1024px and 8192px.'] = 'Á milli 1024px og 8192px.';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'Stærri áferð mun taka lengri tíma að hlaða en smærri áferð mun leiða til óskýrrar kúlu.';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'Birta hringmyndartákn á smámyndalista';
+$lang['Display help message'] = 'Birta hjálparskilaboð';
+$lang['Drag and drop to navigate in the photo.'] = 'Dragðu og slepptu til að fletta í myndinni.';
+$lang['Fullscreen'] = 'Fullskjár';
+$lang['Loading'] = 'Hleðsla';
+$lang['Set Photo Sphere'] = 'Stilltu hringmynd';
