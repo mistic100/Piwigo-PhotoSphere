b08f20e8eab9f969594c29eae562dd9dba934ff0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-09-03 06:29:49 +0200

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Automatic rotation'] = '自動旋轉';
+$lang['Automatically launch animation'] = '自動開始動畫效果';
+$lang['Between 1024px and 8192px.'] = '在 1024 像素到 8192 像素之間。';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = '較大的材質需要的載入時間較長，但較小的材質會看起來比較糊。';
+$lang['Display Photo Sphere icon on thumbnails list'] = '在縮圖列表中顯示 Photo Sphere 的圖示';
+$lang['Display help message'] = '顯示說明訊息';
+$lang['Drag and drop to navigate in the photo.'] = '利用拖拉的方式瀏覽照片。';
+$lang['Fullscreen'] = '全螢幕';
+$lang['Move down'] = '向下移動';
+$lang['Move left'] = '向左移動';
+$lang['Move right'] = '向右移動';
+$lang['Move up'] = '向上移動';
+$lang['Set Photo Sphere'] = '設定 Photo Sphere';
+$lang['Sphere texture size'] = '球體材質大小';
+$lang['Unset Photo Sphere'] = '取消 Photo Sphere';
+$lang['Zoom'] = '縮放';
+$lang['Zoom in'] = '放大';
+$lang['Zoom out'] = '縮小';
+$lang['photosphere_config_intro'] = '若您想將某張照片加入 Photo Sphere 中，請先在照片的屬性頁面中勾選 "Photo Sphere"。您也可以設用批次管理員，或是 Admin Tools 插件中的 Quick Edit。';
