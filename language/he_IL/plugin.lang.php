f12ccf339af5a81d2cd10b4c74661e05776492b2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:55:28 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Automatic rotation'] = 'סיבוב אוטומטי';
+$lang['Automatically launch animation'] = 'הפעלת אנימציה אוטומטית';
+$lang['Between 1024px and 8192px.'] = 'בין 1024 פיקסל ל-8192 פיקסל.';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = 'טקסטורות גדולות ייקחו זמן רב יותר לטעינה, אך טקסטורות קטנות יגרמו לכדור להיות כהה.';
+$lang['Display Photo Sphere icon on thumbnails list'] = 'הצג סמל Photo Sphere ברשימת התמונות הממוזערות';
+$lang['Display help message'] = 'הצג הודעת עזרה';
+$lang['Drag and drop to navigate in the photo.'] = 'גרור ושחרר כדי לנווט בתמונה.';
+$lang['Fullscreen'] = 'מסך מלא';
+$lang['Move down'] = 'הזז למטה';
+$lang['Move left'] = 'הזז שמאלה';
+$lang['Move right'] = 'הזז ימינה';
+$lang['Move up'] = 'הזז למעלה';
+$lang['Set Photo Sphere'] = 'הגדר Photo Sphere';
+$lang['Sphere texture size'] = 'גודל טקסטורת כדור';
+$lang['Unset Photo Sphere'] = 'בטל Photo Sphere';
+$lang['Zoom'] = 'התקרב';
+$lang['Zoom in'] = 'התקרב יותר';
+$lang['Zoom out'] = 'התרחק';
+$lang['photosphere_config_intro'] = 'כדי להשתמש ב-Photo Sphere בתמונה, סמן "Photo Sphere" בדף המאפיינים שלה. ניתן גם להשתמש במנהל הצפייה המהירה של תוסף כלים המנהל.';
