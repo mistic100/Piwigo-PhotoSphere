338708f4f3165ef4916233a6136de3e0a101a2e4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-03 19:37:05 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Automatic rotation'] = '자동 회전';
+$lang['Automatically launch animation'] = '애니메이션 자동 실행';
+$lang['Between 1024px and 8192px.'] = '1024px에서 8192px 사이.';
+$lang['Bigger textures will take longer to load but smaller textures will result in blurry sphere.'] = '텍스처가 클수록 로딩 시간이 길어지지만, 작을수록 스피어 이미지가 흐릿해집니다.';
+$lang['Display Photo Sphere icon on thumbnails list'] = '썸네일 목록에 포토 스피어 아이콘 표시';
+$lang['Display help message'] = '도움말 메시지 표시';
+$lang['Drag and drop to navigate in the photo.'] = '사진 내 이동을 위해 드래그 앤 드롭';
+$lang['Fullscreen'] = '전체 화면';
+$lang['Move down'] = '아래로 이동';
+$lang['Move left'] = '왼쪽으로 이동';
+$lang['Move right'] = '오른쪽으로 이동';
+$lang['Move up'] = '위로 이동';
+$lang['Set Photo Sphere'] = '포토 스피어 설정';
+$lang['Sphere texture size'] = '스피어 텍스처 크기';
+$lang['Unset Photo Sphere'] = '포토 스피어 해제';
+$lang['Zoom'] = '확대/축소';
+$lang['Zoom in'] = '확대';
+$lang['Zoom out'] = '축소';
+$lang['photosphere_config_intro'] = '사진에 포토 스피어를 적용하려면 해당 사진의 속성 페이지에서 "포토 스피어"를 선택하세요. 배치 관리자나 Admin Tools 플러그인의 빠른 편집 기능을 통해서도 설정할 수 있습니다.';
